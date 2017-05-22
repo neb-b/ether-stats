09ce3bc71a2e1afdf01c05d91b1e706d9026fe80
Sean Yesmunt <sean.yesmunt@gmail.com>
2017-05-21 21:49:25 -0700

    use prettier

--- pages/index.js
+++ pages/index.js
@@ -5,52 +5,69 @@ const API = 'https://ether-stats-server-wbffcbisdb.now.sh'
 const WALLET = 'be6Ab449bBa5E9e8E5A81d76D860EFcB4Acaa10F'
 
 class App extends Component {
-  constructor() {
-    super()
+	constructor() {
+		super()
 
-    this.state = {
-      loading: true,
-      error: false,
-      data: {}
-    }
-  }
-  componentDidMount() {
-    axios.get(`${API}/${WALLET}`)
-      .then(({ data }) => {
-        this.setState({ data, loading: false })
-      })
-      .catch((err) => {
-        console.log('err', err)
-        this.setState({ loading: false, error: err })
-      })
-  }
+		this.state = {
+			loading: true,
+			error: false,
+			data: {}
+		}
+	}
+	componentDidMount() {
+		axios
+			.get(`${API}/${WALLET}`)
+			.then(({ data }) => {
+				this.setState({ data, loading: false })
+			})
+			.catch(err => {
+				console.log('err', err)
+				this.setState({ loading: false, error: err })
+			})
+	}
 
-  render() {
-    const { loading, error, data: { hashRate, ethPerMin, usdPerMin, unpaid } } = this.state
+	render() {
+		const {
+			loading,
+			error,
+			data: { hashRate, ethPerMin, usdPerMin, unpaid }
+		} = this.state
 
-    // not sure why they give us unpaid in this format
-    const unpaidEth = unpaid / 1000000000000000000
-    const minutesForOneEther = 1 / ethPerMin
-    const hoursForOneEther = minutesForOneEther / 60
-    const daysForOneEther = (hoursForOneEther / 24).toFixed(0)
+		// not sure why they give us unpaid in this format
+		const unpaidEth = unpaid / 1000000000000000000
+		const minutesForOneEther = 1 / ethPerMin
+		const hoursForOneEther = minutesForOneEther / 60
+		const daysForOneEther = (hoursForOneEther / 24).toFixed(0)
 
-    return (
-      <div className="container">
-        {error && error.message}
-        <h1>Ethminer stats</h1>
-        <h2>Wallet: {WALLET}</h2>
-        {loading && <p>Loading...</p>}
-        {!loading && (
-          <div className="stats">
-            <div className="hash-rate">{hashRate}</div>
-            <div>Currently mining <strong>{ethPerMin}</strong> eth / minute</div>
-            <div>At the current rate, it will take <strong>{daysForOneEther} days</strong> to mine <strong>1 eth</strong></div>
-            <div>Currently mining ${usdPerMin} / min</div>
+		return (
+			<div className="container">
+				{error && error.message}
+				<h1>Ethminer stats</h1>
+				<h2>Wallet: {WALLET}</h2>
+				{loading && <p>Loading...</p>}
+				{!loading &&
+					<div className="stats">
+						<div className="hash-rate">{hashRate}</div>
+						<div>
+							Currently mining <strong>{ethPerMin}</strong> eth / minute
+						</div>
+						<div>
+							At the current rate, it will take
+							{' '}
+							<strong>{daysForOneEther} days</strong>
+							{' '}
+							to mine
+							{' '}
+							<strong>1 eth</strong>
+						</div>
+						<div>Currently mining ${usdPerMin} / min</div>
 
-            <div className="unpaid">Unpaid balance: <strong>{unpaidEth}</strong> eth</div>
-          </div>
-        )}
-        <style jsx>{`
+						<div className="unpaid">
+							Unpaid balance: <strong>{unpaidEth}</strong> eth
+						</div>
+					</div>}
+				<style jsx>
+					{`
             .container {
               padding: 10px;
               font-family: Helvetica, sans-serif;
@@ -76,10 +93,10 @@ class App extends Component {
               font-size: 1.5em;
             }
           `}
-        </style>
-      </div>
-    )
-  }
+				</style>
+			</div>
+		)
+	}
 }
 
 export default App
